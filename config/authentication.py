13d17d93297462dbc021bf8fccb16f4cbe30bc1e
Dogmin <kdm3578@gmail.com>
2022-01-05 21:04:28 +0900

    authentication part 1

---
+++ config/authentication.py
@@ -0,0 +1,19 @@
+from django.conf import settings
+from users.models import User
+from rest_framework import authentication, exceptions
+import jwt
+
+
+class JWTAuthentication(authentication.BaseAuthentication):
+    def authenticate(self, request):
+        try:
+            token = request.META.get("HTTP_AUTHORIZATION")
+            if token is None:
+                return None
+            xjwt, jwt_token = token.split(" ")
+            decoded = jwt.decode(jwt_token, settings.SECRET_KEY, algorithms=["HS256"])
+            pk = decoded.get("pk")
+            user = User.objects.get(pk=pk)
+            return (user, None)
+        except (ValueError, jwt.exceptions.DecodeError, User.DoesNotExist):
+            return None
