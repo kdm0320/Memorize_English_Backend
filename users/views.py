c5054035491b024fc0d9d104f33a5c23a8c2dbc0
Dogmin <kdm3578@gmail.com>
2022-01-05 23:57:23 +0900

    user login

--- users/views.py
+++ users/views.py
@@ -1,8 +1,16 @@
+from django.contrib.auth import authenticate
+from django.conf import settings
+from rest_framework import status
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from rest_framework import permissions
+
 from .serializers import UserSerializer
 from .models import User
 from .permissions import IsSelf
+
+import jwt
 
 
 class UserViewSet(ModelViewSet):
@@ -18,3 +26,20 @@ class UserViewSet(ModelViewSet):
         else:
             permission_classes = [IsSelf]
         return [permission() for permission in permission_classes]
+
+    @action(detail=False, methods=["POST"])
+    def login(self, request):
+        username = request.data.get("username")
+        password = request.data.get("password")
+
+        if not username or not password:
+            return Response(statue=status.HTTP_400_BAD_REQUEST)
+
+        user = authenticate(username=username, password=password)
+        if user:
+            encoded_jwt = jwt.encode(
+                {"pk": user.pk}, settings.SECRET_KEY, algorithm="HS256"
+            )
+            return Response(data={"token": encoded_jwt, "id": user.pk})
+        else:
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
