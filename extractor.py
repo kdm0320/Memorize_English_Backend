2c47d86e34427a328cff1dd9eb16b722a1acc932
Dongmin <kdm3578@gmail.com>
2022-01-20 22:41:52 +0900

    Update_6 유저학습창(first)

--- extractor.py
+++ extractor.py
@@ -10,7 +10,7 @@ from words import models
 
 def convert_data():
     data = openpyxl.load_workbook(
-        "C:/Users/User/Desktop/Dongmin/Project/memorize_english/datas/words/TOEIC 토익 빈출 영어단어정리 327.xlsx"
+        "/Users/gimdongmin/Desktop/memorize_english/datas/words/TOEIC 토익 빈출 영어단어정리 327.xlsx"
     )
     ws = data.active
     words = ws["B"] + ws["E"]
@@ -20,10 +20,8 @@ def convert_data():
     for i in range(len(words)):
         if str(words[i].value) != "None" or str(meaning[i].value) != "None":
             all_value.append(
-                (
-                    unicodedata.normalize("NFKD", str(words[i].value)),
-                    unicodedata.normalize("NFKD", str(meaning[i].value)),
-                )
+                {unicodedata.normalize("NFKD", str(words[i].value)):unicodedata.normalize("NFKD", str(meaning[i].value))}
+                
             )
     models.Word.objects.create(title=title, content=all_value)
 
