6024995d51736c07bbb8d3b972ed9f7e8a73551f
Dongmin <kdm3578@gmail.com>
2022-02-10 14:38:15 +0900

    requirements 추가 / netlify 배포

--- config/settings.py
+++ config/settings.py
@@ -47,7 +47,6 @@ PROJECT_APPS = [
 THIRD_PARTY_APPS = [
     "rest_framework",
     "corsheaders",
-    'webpack_loader',
 ]
 
 INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS + THIRD_PARTY_APPS
@@ -132,9 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = "static/"
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'client', 'build','static')
-]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
