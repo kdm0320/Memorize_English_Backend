22fdc0521b0b9f61bdea11e9deda92b78182506a
Dongmin <kdm3578@gmail.com>
2022-02-14 09:49:18 +0900

    update

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = bool(env('DEBUG'))
 DEBUG = True
-ALLOWED_HOSTS = ["*.herokuapp.com"]
+ALLOWED_HOSTS = ["*"]
 CORS_ALLOW_METHODS = (
     'DELETE',
     'GET',
